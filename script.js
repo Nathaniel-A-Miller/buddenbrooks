4b8a5e02c471352e7c6727a042e5d025d1299a16
Nathaniel Ashton Miller <65291011+Nathaniel-A-Miller@users.noreply.github.com>
2025-10-24 08:43:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // 0. FIREBASE CONFIGURATION (PASTE YOUR CONFIG HERE)
 // ========================================================================
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
